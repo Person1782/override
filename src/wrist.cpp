cc30e12653c66e9c5c3d504e06376505c70dd9aa
Rishab Manas <rishabmsahu748@gmail.com>
2026-07-20 15:47:37 -0700

    readability for wrist

--- src/wrist.cpp
+++ src/wrist.cpp
@@ -3,12 +3,12 @@
 void wrist_logic() {
     double lift_position = (lift.get_position(0) + lift.get_position(1)) / 2.0; // Average position of the lift motors
     if(lift_position <= 5){
-        wrist.set(false); // Set wrist to down position
+        wrist.set(false); // Set wrist to retracted position
     } else {
-        wrist.set(true); // Set wrist to up position
+        wrist.set(true); // Set wrist to extended position
     }
 }
 
 void wrist_opcontrol() {
-    wrist.set(true);
+    wrist.set(true); // Set wrist to extended position
 }
