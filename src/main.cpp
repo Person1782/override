98e200387099cfe5ba1e0abfbedc2264743e3b17
Rishab Manas <rishabmsahu748@gmail.com>
2026-07-20 14:38:20 -0700

    changed drivetrain motors again to correct ports

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 // Chassis constructor
 ez::Drive chassis(
     // These are your drive motors, the first motor is used for sensing!
-    {3, 4},     // Left Chassis Ports (negative port will reverse it!)
+    {-3, -4},     // Left Chassis Ports (negative port will reverse it!)
     {1, 2},  // Right Chassis Ports (negative port will reverse it!)
 
     9,      // IMU Port
