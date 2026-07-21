937b19256ce5fd672d8dfbdb9ae54834efb4f321
Rishab Manas <rishabmsahu748@gmail.com>
2026-07-20 17:01:43 -0700

    more wrist override fixes

--- src/wrist.cpp
+++ src/wrist.cpp
@@ -1,7 +1,7 @@
 #include "main.h"
 
 void wrist_logic() {
-    if (!wrist_macro_active) {
+    if (wrist_macro_active) {
         double lift_position = (lift.get_position(0) + lift.get_position(1)) / 2.0; // Average position of the lift motors
         if(lift_position <= 5){
             wrist.set(false); // Set wrist to retracted position
@@ -12,13 +12,13 @@ void wrist_logic() {
 }
 
 void wrist_opcontrol() {
-    wrist.button_toggle(master.get_digital(DIGITAL_UP));
+    if(!wrist_macro_active){
+        wrist.button_toggle(master.get_digital(DIGITAL_UP));
+    }
 }
 
 void get_wrist_macro_active() {
-    if(wrist_macro_active){
-        if(master.get_digital(DIGITAL_UP) && master.get_digital(DIGITAL_DOWN)) {
-            wrist_macro_active = !wrist_macro_active; // Toggle the wrist macro active state
-        }
+    if(master.get_digital_new_press(DIGITAL_UP) && master.get_digital(DIGITAL_DOWN)) {
+        wrist_macro_active = !wrist_macro_active; // Toggle the wrist macro active state
     }
 }
